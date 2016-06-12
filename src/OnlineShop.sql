989073fd9ee194d994e7afebf3c553e265a69241
mat-eyo <maciej.matejko@wp.eu>
2016-06-12 21:07:09 +0200

    add column 'stock' to the Item table in db

--- src/OnlineShop.sql
+++ src/OnlineShop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Czas wygenerowania: 10 Cze 2016, 15:12
+-- Czas wygenerowania: 12 Cze 2016, 19:05
 -- Wersja serwera: 5.5.49-0ubuntu0.14.04.1
 -- Wersja PHP: 5.5.9-1ubuntu4.16
 
@@ -23,20 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `Admin`
---
-
-CREATE TABLE IF NOT EXISTS `Admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mail` varchar(80) COLLATE utf8_polish_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `mail` (`mail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Struktura tabeli dla tabeli `Item`
 --
 
@@ -45,117 +31,10 @@ CREATE TABLE IF NOT EXISTS `Item` (
   `name` varchar(60) COLLATE utf8_polish_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
   `price` decimal(9,2) NOT NULL,
+  `stock` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `Messages`
---
-
-CREATE TABLE IF NOT EXISTS `Messages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `admin_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `order_id` int(11) DEFAULT NULL,
-  `text` text COLLATE utf8_polish_ci,
-  PRIMARY KEY (`id`),
-  KEY `admin_id` (`admin_id`),
-  KEY `user_id` (`user_id`),
-  KEY `order_id` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `Order`
---
-
-CREATE TABLE IF NOT EXISTS `Order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `status` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `Pictures`
---
-
-CREATE TABLE IF NOT EXISTS `Pictures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(2083) COLLATE utf8_polish_ci NOT NULL,
-  `item_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `item_id` (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `Product_Order`
---
-
-CREATE TABLE IF NOT EXISTS `Product_Order` (
-  `item_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  KEY `item_id` (`item_id`),
-  KEY `order_id` (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `User`
---
-
-CREATE TABLE IF NOT EXISTS `User` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) COLLATE utf8_polish_ci NOT NULL,
-  `surname` varchar(60) COLLATE utf8_polish_ci NOT NULL,
-  `email` varchar(80) COLLATE utf8_polish_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `address` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `Messages`
---
-ALTER TABLE `Messages`
-  ADD CONSTRAINT `Messages_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `Admin` (`id`),
-  ADD CONSTRAINT `Messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`),
-  ADD CONSTRAINT `Messages_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `Order` (`id`);
-
---
--- Ograniczenia dla tabeli `Order`
---
-ALTER TABLE `Order`
-  ADD CONSTRAINT `Order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE;
-
---
--- Ograniczenia dla tabeli `Pictures`
---
-ALTER TABLE `Pictures`
-  ADD CONSTRAINT `Pictures_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `Item` (`id`) ON DELETE CASCADE;
-
---
--- Ograniczenia dla tabeli `Product_Order`
---
-ALTER TABLE `Product_Order`
-  ADD CONSTRAINT `Product_Order_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `Item` (`id`),
-  ADD CONSTRAINT `Product_Order_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `Order` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
